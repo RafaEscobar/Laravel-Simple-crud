6171786c2e1afe4fca7b5535a7b89f207988c7c1
Rafa Escobar <Rafael.EG.code@gmail.com>
2025-06-20 17:16:11 -0600

    Agrega modal para agregar un nuevo autor

--- resources/views/components/simple-modal.blade.php
+++ resources/views/components/simple-modal.blade.php
@@ -1,14 +1,16 @@
 <div>
-    <button id="btnOpen" class="px-4 py-3">
+    <button id="btnOpen">
         {{ $btn }}
     </button>
     <div id="modal" class="hidden fixed inset-0 items-center justify-center z-50">
         <div class="absolute inset-0 bg-black opacity-50"></div>
         <div class="relative bg-white p-6 rounded-lg shadow-lg w-1/3 text-center">
-            <h2 class="text-2xl font-semibold mb-4">{{ $title }}</h2>
+            <div class="flex justify-between">
+                <span class="text-2xl font-semibold mb-4">{{ $title }}</span>
+                <span id="btnClose"><x-fas-circle-xmark class="w-6 text-gray-500 cursor-pointer" /></span>
+            </div>
             {{ $body }}
             {{ $footer }}
         </div>
     </div>
-
 </div>
