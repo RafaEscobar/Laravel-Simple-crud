2ca5e462d01d63078244c622e203c5a1ff8b32b2
Rafa Escobar <Rafael.EG.code@gmail.com>
2025-06-20 18:13:07 -0600

    Limpiando rutas

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BookController::class, 'index'])->name('welcome');
 
-Route::resource('/authors', AuthorController::class);
-Route::resource('/books', BookController::class)->except('index');
+Route::resource('/authors', AuthorController::class)->only(['index','store', 'update', 'destroy']);
+Route::resource('/books', BookController::class)->only(['store', 'update', 'destroy']);
